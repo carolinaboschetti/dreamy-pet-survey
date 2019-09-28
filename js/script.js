edd6b4e0f1810fdbf161efd044a804b4cba10377
Carolina Boschetti <30893634+carolinaboschetti@users.noreply.github.com>
2019-09-28 13:43:04 +0200

    children_crossing: Improving usability
    cd

--- js/script.js
+++ js/script.js
@@ -8,28 +8,38 @@ inputs.forEach(function(input) {
     })
     input.addEventListener("blur", function(event) {
         const element = event.target
+        element.setCustomValidity("")
         let isValid = element.checkValidity()
-        let errorMsg = ""
-        switch (element.name) {
-            case "name":
-                errorMsg = "This field should be your name."
-                break;
-            case "email":
-                errorMsg = "We need your E-mail to avoid duplication."
-                break;
-            case "age":
-                if (element.value < 18) {
-                    errorMsg = "Sorry, we can't accept your application."
-                } else if (element.validity.valueMissing) {
-                    errorMsg = "We need to know how old you are."
-                }
-                break;
-            default:
-                errorMsg = "Please verify this field."
-                break;
+        
+        if (!isValid) {
+            element.setCustomValidity(setErrorMsg(element))
+            element.classList.add("invalid")
+        } else  {
+            element.setCustomValidity("")
+            element.classList.remove("invalid")
         }
-        if (!isValid) element.setCustomValidity(errorMsg)
-      console.log(`is ${input.name} valid? ${isValid}`)
+        console.log(`is ${input.name} valid? ${isValid}`)
     })
   })
-  
+  const setErrorMsg = (element) => {
+    let errorMsg
+    switch (element.name) {
+        case "name":
+            errorMsg = "This field should be your name."
+            break
+        case "email":
+            errorMsg = "We need your E-mail to avoid duplication."
+            break
+        case "age":
+            if (element.value < 18) {
+                errorMsg = "Sorry, we can't accept your application."
+            } else if (element.validity.valueMissing) {
+                errorMsg = "We need to know how old you are."
+            }
+            break
+        default:
+            errorMsg = "Please verify this field."
+            break
+    }
+    return errorMsg 
+  }
